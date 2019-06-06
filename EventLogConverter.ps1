739b72b165c812849a3e8113d5c9ae72126351fd
Stuart Squibb <stuart@squibbs.net>
2019-06-06 17:23:25 +0100

    Added initial script

---
+++ EventLogConverter.ps1
@@ -0,0 +1,76 @@
+Function ConvertTo-EvtObject  {
+    [cmdletbinding()]
+    Param (
+    [parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+    [Object[]]$InputObject
+    )
+    Process {
+        $Objects = @()
+        $InputObject | ForEach-Object { 
+            $xdoc =  [xml]$_.ToXML()
+            $obj = New-Object PSObject
+            #create NoteProperty for each System node - check for attributes and create
+            #a new property using a <node><attribute> syntax
+            $xdoc.Event.System.ChildNodes | ForEach-Object {
+                if ($_.HasAttributes) {
+                    $parentName  = $_.LocalName
+                    $_.Attributes | ForEach-Object {
+                        $obj | Add-Member -MemberType NoteProperty -Name "$($parentName)$($_.Name)" -Value $_.'#text'
+                    }
+                }
+                if ($_.'#text') {
+                    #add the Text node if present
+                    $obj | Add-Member -MemberType NoteProperty -Name $_.LocalName -Value $_.InnerText 
+                }
+            }
+            #check for named EventData node(s)
+            $xdoc.Event.EventData  | ForEach-Object {
+                if ($_.Name -ne $_.LocalName) {
+                    $PropertyName = "EventDataName"
+                    $PropertyValue = $_.Name
+                    $obj | Add-Member -MemberType NoteProperty -Name $PropertyName -Value $PropertyValue
+                }
+            }
+            #create NoteProperty for each EventData.Data node
+            $DataCounter = 0
+            if ($xdoc.Event.EventData) {        
+                $xdoc.Event.EventData.Data | ForEach-Object { 
+                    if ($_.Name) {
+                        $PropertyName = $_.Name
+                        $PropertyValue = $_.innerText
+                    }
+                    else {
+                        #for unnamed Data nodes, use a numeric counter
+                        $PropertyName = "Data$($DataCounter.toString('00'))"
+                        $PropertyValue =  $_.toString()
+                        $DataCounter += 1
+                    }
+                    $obj | Add-Member -MemberType NoteProperty -Name $PropertyName -Value $PropertyValue
+                }
+            }
+            #if there is a Userdata node, try to handle that
+            if ($xdoc.Event.UserData) {        
+                $xdoc.Event.UserData | ForEach-Object { 
+                    $PropertyName = 'UserData'
+                    $PropertyValue = $_.InnerXML
+                    $obj | Add-Member -MemberType NoteProperty -Name $PropertyName -Value $PropertyValue
+                }
+            }
+            #if there is a Binary node, try to handle that
+            if ($xdoc.Event.EventData.Binary) { 
+                $PropertyName = "Binary"
+                $PropertyValue =  $xdoc.Event.EventData.Binary.toString()
+                $obj | Add-Member -MemberType NoteProperty -Name $PropertyName -Value $PropertyValue
+            }
+            $Objects += $obj
+        }
+        $Objects
+    }
+}
+
+#a small test - change the LogName and Filter to suit
+#$events = Get-WinEvent -LogName Application -FilterXPath "*[System[EventID=0]]" -MaxEvents 1
+#$events = Get-WinEvent -LogName Security -FilterXPath "*[System[EventID=4624]]" -MaxEvents 1
+#$events = Get-WinEvent -LogName Application -FilterXPath "*[System[EventID=1000]]" -MaxEvents 1
+$events = Get-WinEvent -LogName Application -FilterXPath "*[UserData]" -MaxEvents 1000
+ConvertTo-EvtObject $events
