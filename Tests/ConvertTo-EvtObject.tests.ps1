9625e4df300644a8863bdb79be547400ade8e695
Stuart Squibb <stuart@squibbs.net>
2019-07-11 19:37:05 +0100

    Modified Pester Test

--- Tests/ConvertTo-EvtObject.tests.ps1
+++ Tests/ConvertTo-EvtObject.tests.ps1
@@ -9,7 +9,7 @@ Describe "EventLogConverter Tests" {
     #Sample Event representing a power-on. Should be present on all machines.
     $sampleEvent = Get-WinEvent -LogName 'System' -FilterXPath "*[System[EventID=12 and Provider[@Name='Microsoft-Windows-Kernel-General']]]" -MaxEvents 1 | ConvertTo-EvtObject
     It "Returns 1 Event ID 12 event (Kernel-General)" {            
-        ($sampleEvent | Measure-Object).Count | Should Be 1
+        ($sampleEvent | Measure-Object) | Should -HaveCount 1
     }
     It "Event has 'ProviderName' property set to 'Microsoft-Windows-Kernel-General'" {
         $sampleEvent.ProviderName  | Should be 'Microsoft-Windows-Kernel-General'
